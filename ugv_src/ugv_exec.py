5c7c890b79500d5135b10f23a5d67ab073c0c4bc
jdavis8061 <jarrett.davis@bulldogs.aamu.edu>
2025-02-12 20:50:56 -0600

    Added Initialization Functions

--- ugv_src/ugv_exec.py
+++ ugv_src/ugv_exec.py
@@ -1,11 +1,38 @@
+#import asyncio
+#import logging
+#from mavsdk import System
+
+def initialize_camera():
+    print("Hello camera")
+
+def initialize_pixhawk():
+    print("Hello Pixhawk")
+#    drone = System()
+#    await drone.connect()
+
+def initialize_wifi():
+    print("Hello Wifi")
+
+def initialize_mp_params():
+    print("Hello Mission Planner")
+
+def initialize_logger():
+    print("Hello logger")
+
 def initialize():
     print("initialize")
+    initialize_logger()
+    initialize_pixhawk()
+    initialize_wifi()
+    initialize_camera()
+    initialize_mp_params()
 
 def run():
     print("run")
 
 def finalize():
     print("finalize")
+
 
 print("Hello UGV World")
 initialize()
