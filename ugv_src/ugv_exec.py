a737d74c1fc4179b9a64c8cdcc5bde27ccce7c89
jdavis8061 <jarrett.davis@bulldogs.aamu.edu>
2025-02-17 18:48:31 -0600

    Added OpenCV Camera Function

--- ugv_src/ugv_exec.py
+++ ugv_src/ugv_exec.py
@@ -1,18 +1,42 @@
+
+import cv2
 #import asyncio
 #import logging
 #from mavsdk import System
+#import ArducamDepthCamera
+#print(dir(ArducamDepthCamera.Connection))
 #from ArducamDepthCamera import ArducamCamera, Connection
 
-def initialize_camera():
+def initialize_camera_DC():
     print("Hello camera")
     #Creates an instance of the camera
-    #camera = ArducamCamera()
-    #ret = camera.open(Connection.MODE_USB)
-    #if ret == 0:
-    #    print("Camera initialized successfully.")
-    #else:
-    #    print(f"Failed to initialize camera. Error code: {ret}")
+    
+    camera = ArducamCamera()
+    ret = camera.open(Connection.USB)
+    if ret == 0:
+        print("Camera initialized successfully.")
+    else:
+        print(f"Failed to initialize camera. Error code: {ret}")
+    
+def initialize_camera_cv2():
+    cap = cv2.VideoCapture(0)
 
+    if not cap.isOpened():
+        print("❌ Failed to open camera")
+        exit()
+#move to run later
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("❌ Failed to grab frame")
+            break
+
+        cv2.imshow("Camera Feed", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+#move to finalize later
+    cap.release()
+    cv2.destroyAllWindows()
 
 def initialize_pixhawk():
     print("Hello Pixhawk")
@@ -28,7 +52,7 @@ def initialize_mp_params():
 def initialize_logger():
     print("Hello logger")
 
-def initialize():
+"""def initialize():
     print("initialize")
     initialize_logger()
     initialize_pixhawk()
@@ -47,3 +71,5 @@ print("Hello UGV World")
 initialize()
 run()
 finalize()
+"""
+initialize_camera_cv2()
