fc9a83afc1a6c6b2910ce48be9edf23eee922271
Jarrett Davis <jarrett.davis@bulldogs.aamu.edu>
2025-03-19 18:29:14 -0500

    added actuator.py as official actuator call file for competition

---
+++ ugv_src/actuator.py
@@ -0,0 +1,30 @@
+import serial
+import time
+
+#list min: 992
+#listed max: 2000
+#actual min: 1040
+#actual max: 1950
+
+SERIAL_PORT = "/dev/serial/by-id/usb-Pololu_Corporation_Pololu_Micro_Maestro_6-Servo_Controller_00454274-if00"
+BAUD_RATE = 9600
+
+def send_command(channel, target):
+    command = bytearray([0x84, channel, target & 0x7F, (target >> 7) & 0x7F])
+    with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
+        ser.write(command)
+        time.sleep(0.1)
+
+def push():
+    print("Activating Servo...")
+    send_command(0, 8000)
+    time.sleep(7)
+
+def retract():
+    print("Retracting Servo...")
+    send_command(0, 4000)
+    time.sleep(7)
+
+def push_then_retract():
+    push()
+    retract()
