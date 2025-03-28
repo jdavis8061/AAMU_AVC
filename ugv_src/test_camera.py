d7fe4983c8a529f7a8808a2278e7d950beec311e
Jarrett Davis <jarrett.davis@bulldogs.aamu.edu>
2025-03-27 22:29:35 -0500

    Updated Camera Test File

--- ugv_src/test_camera.py
+++ ugv_src/test_camera.py
@@ -1,34 +1,98 @@
-import cv2
+import cv2 as cv
+from cv2 import aruco
 import numpy as np
-print(np.__version__)
-print(cv2.__version__)
+import actuator
+import os
+import time
 
-def list_cameras():
-    print("Scanning for available cameras...")
-    for i in range(10):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            print(f"✅ Camera detected at index: {i}")
-            cap.release()
-        else:
-            print(f"❌ No camera at index: {i}")
+calib_data_path = os.path.join(os.path.dirname(__file__), "..", "MultiMatrix.npz")
 
-#list_cameras()
-cap = cv2.VideoCapture(1) #Change to 0 when using Raspberry Pi, 1 when using laptop
+calib_data = np.load(calib_data_path)
+print(calib_data.files)
+cam_mat = calib_data["camMatrix"]
+dist_coef = calib_data["distCoef"]
+r_vectors = calib_data["rVector"]
+t_vectors = calib_data["tVector"]
 
-if not cap.isOpened():
-    print("❌ Failed to open camera")
-    exit()
+
+MARKER_SIZE = 8  
+
+marker_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+
+param_markers = aruco.DetectorParameters()
+
+#cap = cv.VideoCapture("http://10.235.100.3:8080/video") #give the server id shown in IP webcam App
+cap = cv.VideoCapture(0) #uses USB Camera Added (Change to 0 when using Raspberry Pi, 1 when using laptop)
+push_complete = False
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ Failed to grab frame")
         break
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+        gray_frame, marker_dict, parameters=param_markers
+    )
+    if marker_corners:
+        rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+            marker_corners, MARKER_SIZE, cam_mat, dist_coef
+        )
+        total_markers = range(0, marker_IDs.size)
+        for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+            print(ids[0])
+            if not push_complete and ids[0] == 23:
+                print("correct marker detected")
+                actuator.push_then_retract()
+                push_complete = True
+                cap.release()
+                cv.destroyAllWindows()
+                break
+            cv.polylines(
+                frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+            )
+            corners = corners.reshape(4, 2)
+            corners = corners.astype(int)
+            top_right = corners[0].ravel()
+            top_left = corners[1].ravel()
+            bottom_right = corners[2].ravel()
+            bottom_left = corners[3].ravel()
 
-    cv2.imshow("Camera Feed", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+            
+            # calculate the distance
+            distance = np.sqrt(
+                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
+            )
+
+
+
+            # for pose of the marker
+            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
+            cv.putText(
+                frame,
+                f"id: {ids[0]} Dist: {round(distance, 2)}",
+                top_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.3,
+                (0, 0, 255),
+                2,
+                cv.LINE_AA,
+                
+            )
+            cv.putText(
+                frame,
+                f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
+                bottom_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.0,
+                (0, 0, 255),
+                2,
+                cv.LINE_AA,
+            )
+            # print(ids, "  ", corners)
+    cv.imshow("frame", frame)
+    key = cv.waitKey(1)
+    if key == ord("q"):
         break
-
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
