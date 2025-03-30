18f1a672dfe35b76c36f7eb69399cc6ca16ed5d7
Your Name <you@example.com>
2025-03-30 17:03:38 -0500

    added file to get the gps coordinates

--- uav_src/test_get_coordinates.py
+++ uav_src/test_get_coordinates.py
@@ -1,17 +1,16 @@
 from pymavlink import mavutil
 
-#must be connected to Pixhawk, GPS, and Power Supply to run this code
-master=mavutil.mavlink_connection('/dev/serial0', baud=57600)
-
-master.wait_heartbeat()
-print("Connected to Vehicle")
+#must be connected to Pixhawk, GPS, and Power Supply to run this code 
 def get_gps():
+    master=mavutil.mavlink_connection('/dev/serial0', baud=57600)
+    master.wait_heartbeat()
+    print("Connected to Vehicle")
     while True:
         msg = master.recv_match(type='GPS_RAW_INT', blocking=True)
         if msg:
-            aruco_latitude = msg.lat / 1e7
-            aruco_longitude = msg.lon / 1e7
-            print(f'ArUco Lat:{aruco_latitude}, aruco longitude:{aruco_longitude}')
-            return aruco_latitude, aruco_longitude
+            gps_latitude = msg.lat / 1e7
+            gps_longitude = msg.lon / 1e7
+            print(f'ArUco Lat:{gps_latitude}, aruco longitude:{gps_longitude}')
+            return gps_latitude, gps_longitude
 
-aruco_latitude, aruco_longitude = get_gps()
+drone_latitude, drone_longitude = get_gps()
